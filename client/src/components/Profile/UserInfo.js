6c036a2e7f127fb96ec7e036f116190849d2256f
bczaderna <bczaderna@gmail.com>
2019-05-10 12:07:42 -0400

    minor css changes in navbar

--- client/src/components/Profile/UserInfo.js
+++ client/src/components/Profile/UserInfo.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const formatDate = date => {
-  const newDate = new Date(date).toLocaleDateString('en-US');
-  const newTime = new Date(date).toLocaleTimeString('en-US');
+  const newDate = new Date(date).toLocaleDateString("en-US");
+  const newTime = new Date(date).toLocaleTimeString("en-US");
   return `${newDate} at ${newTime}`;
 };
 
 const UserInfo = ({ session }) => (
-  <div className='App'>
-    <h3>User Info</h3>
+  <div className="App">
+    <br />
+    <br />
+    <h3>Your Info</h3>
     <p>Username: {session.getCurrentUser.username}</p>
     <p>Email: {session.getCurrentUser.email}</p>
     <p>Join Date: {formatDate(session.getCurrentUser.joinDate)}</p>
     <ul>
-      <h3>{session.getCurrentUser.username}'s Favorites</h3>
-      {session.getCurrentUser.favorites.map(favorite => (
+      <h3>Your Favorites</h3>
+      {/* {session.getCurrentUser.favorites.map(favorite => (
         <li key={favorite._id}>
           <Link to={`/quizzes/${favorite._id}`}>
             <p>{favorite.name}</p>
@@ -26,7 +28,8 @@ const UserInfo = ({ session }) => (
         <p>
           <strong>You currently have no favorites. Go add some!</strong>
         </p>
-      )}
+      )} */}
+      <h3>Your Gifs</h3>
     </ul>
   </div>
 );
