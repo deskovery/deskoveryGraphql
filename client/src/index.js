fab6ec98fb8f86d896556b58ac4de60940f4d704
Lerena Holloway <holloway.lerena@gmail.com>
2019-05-10 22:22:12 -0400

    Preparation for deployment

--- client/src/index.js
+++ client/src/index.js
@@ -28,7 +28,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
-  uri: `http://localhost:4444/graphql`,
+  uri: `https://desk0very.herokuapp.com/graphql`,
   fetchOptions: {
     credentials: 'include',
   },
