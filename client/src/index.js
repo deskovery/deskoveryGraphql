f55104981770c4846fdeef8034c4d7de2e92e727
Joseph Whittington <joseph.a.whittington@gmail.com>
2019-05-14 10:02:58 -0400

    Changes the GQL API url to production in the build script

--- client/src/index.js
+++ client/src/index.js
@@ -28,7 +28,9 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
-  uri: `http://localhost:4444/graphql`,
+  uri: process.env.NODE_ENV === "production"?
+    `https://desk0very.herokuapp.com/graphql`:
+    `http://localhost:4444/graphql`,
   fetchOptions: {
     credentials: 'include',
   },
