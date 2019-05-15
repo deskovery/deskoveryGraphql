34aa0152c1d2a4881858bb7fd91477cbc3ab3d45
bczaderna <bczaderna@gmail.com>
2019-05-15 01:04:49 -0400

    added endpoints, but there is a problem with sky gifs, added back and next buttons

--- client/src/components/user-home.js
+++ client/src/components/user-home.js
@@ -20,7 +20,9 @@ class UserHome extends Component {
       welcome: true,
       openFacts: false,
       openJournal: false,
-      playVideo: this.props.match.params._id || this.props.location.state.video
+      playVideo: this.props.location.state.video,
+      playVideoIndex: 0,
+      counter: 0
     };
     this.addToFavs = this.addToFavs.bind(this);
   }
@@ -28,6 +30,10 @@ class UserHome extends Component {
   ref = youtube => {
     this.player = youtube;
   };
+
+  componentDidMount() {
+    console.log(this.state.playVideo, "video array");
+  }
 
   addToFavs() {
     let itemsArray = localStorage.getItem("items")
@@ -54,26 +60,51 @@ class UserHome extends Component {
     this.setState({ takeoff: false });
   };
 
+  goBack = () => {
+    let index = this.state.counter - 1;
+
+    this.setState({
+      playVideoIndex: index
+    });
+
+    this.setState({
+      counter: index
+    });
+  };
+
+  goNext = () => {
+    let index = this.state.counter + 1;
+
+    this.setState({
+      playVideoIndex: index
+    });
+    this.setState({
+      counter: index
+    });
+  };
+
   render() {
+    
     setTimeout(() => {
       this.setState({ takeoff: false });
     }, 7800);
     if (this.state.takeoff) {
       return (
         <div>
-        <h1 id="welcomeHeader">Up, up, and away . . .</h1>
-        <div className="takeoff">
-          <img
-            className="takeoffGif"
-            src="https://cdn.dribbble.com/users/1303437/screenshots/3492466/plane_800x600.gif"
-            alt="prepare for takeoff."
-          />
-          <div className="skipContainer">
-            <button className="skip" onClick={this.skipFlight}>
-              Skip
-            </button>
+          <h1 id="welcomeHeader">Up, up, and away . . .</h1>
+          <div className="takeoff">
+            <img
+              className="takeoffGif"
+              src="https://cdn.dribbble.com/users/1303437/screenshots/3492466/plane_800x600.gif"
+              alt="prepare for takeoff."
+            />
+            <div className="skipContainer">
+              <button className="skip" onClick={this.skipFlight}>
+                Skip
+              </button>
+            </div>
           </div>
-        </div></div>
+        </div>
       );
     } else {
       setTimeout(() => {
@@ -81,13 +112,13 @@ class UserHome extends Component {
       }, 2850);
       if (this.state.welcome) {
         return (
-          <div >
+          <div>
             <h1 id="welcomeHeader">Welcome to your destination!</h1>
-            <div id='passport-gif'>
-            <img
-              id="passport-gif"
-              src="https://cdn.dribbble.com/users/1022424/screenshots/3395922/tickets_and_passport_dribble.gif"
-            />
+            <div id="passport-gif">
+              <img
+                id="passport-gif"
+                src="https://cdn.dribbble.com/users/1022424/screenshots/3395922/tickets_and_passport_dribble.gif"
+              />
             </div>
           </div>
         );
@@ -98,8 +129,11 @@ class UserHome extends Component {
               {this.state.openFacts ? (
                 <FactCarousel destination={this.state.playVideo} />
               ) : null}
+
               <YouTubePlayer
-                url={`https://www.youtube.com/watch?v=${this.state.playVideo}`}
+                url={`https://www.youtube.com/watch?v=${
+                  this.state.playVideo[this.state.playVideoIndex]
+                }`}
                 playing={this.state.playing}
                 ref={this.ref}
                 width="70vw"
@@ -111,6 +145,9 @@ class UserHome extends Component {
               {this.state.openJournal ? <Journal /> : null}
             </div>
             <div className="user-home-buttons">
+              <button className="next-button" onClick={this.goBack}>
+                Back
+              </button>
               <ControlledPopup videoSrc={this.state.playVideo} />
               {/* {this.state.videoSrc ? (
                 <Capture videoSrc={this.state.playVideo} />
@@ -121,6 +158,9 @@ class UserHome extends Component {
               </button>
               <button className="user-home-buttons" onClick={this.openFacts}>
                 Facts
+              </button>
+              <button className="next-button" onClick={this.goNext}>
+                Next
               </button>
               <LikeVideo
                 _id={this.state.playVideo}
