dd8cd75d58ef34dedaebe0e657411c843903d7bd
Lerena Holloway <holloway.lerena@gmail.com>
2019-05-15 16:32:00 -0400

    removes likes

--- client/src/components/Video/SearchItem.js
+++ client/src/components/Video/SearchItem.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const SearchItem = ({ videoId, name, likes, imageUrl }) => (
+const SearchItem = ({ videoId, name, imageUrl }) => (
   <li>
     <Link to={`/videos/${videoId}`}>
       <h4>{name}</h4>
-      <img src={imageUrl} alt='pic of vid' />
     </Link>
-    <p>Likes: {likes}</p>
   </li>
 );
 
